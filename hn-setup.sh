19081a8804f8f342c9bfa36b75e50f8182c8f69e
hirtanak <hirtanak@gmail.com>
2018-04-06 08:56:44 +0900

    Update hn-setup.sh

--- hn-setup.sh
+++ hn-setup.sh
@@ -43,7 +43,8 @@ ln -s /mnt/lts /home/$USER/lts
 #Install needed packages
 yum check-update
 yum install -y epel-release
-yum install -y nfs-utils sshpass nmap htop pdsh screen git psmisc
+#yum install -y nfs-utils sshpass nmap htop pdsh screen git psmisc
+yum install -y nfs-utils sshpass nmap pdsh psmisc
 yum install -y gcc libffi-devel python-devel openssl-devel --disableexcludes=all
 #yum groupinstall -y "X Window System"
 
