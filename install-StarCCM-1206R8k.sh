39356a81b3df0d79462f07ced982904530c58777
hirtanak <hirtanak@gmail.com>
2018-04-07 00:06:08 +0900

    Update install-StarCCM-1206R8k.sh

--- install-StarCCM-1206R8k.sh
+++ install-StarCCM-1206R8k.sh
@@ -30,6 +30,8 @@ echo 'export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.223/linux/mpi'
 echo 'export PATH=$SHARE_DATA/applications/12.06.010-R8/STAR-CCM+12.06.010-R8/star/bin:/opt/intel/impi/5.1.3.223/bin64:$PATH' >> $USER/.bashrc
 echo 'export I_MPI_DYNAMIC_CONNECTION=0' >> $USER/.bashrc
 echo '${SHARE_DATA}/applications/12.06.010-R8/STAR-CCM+12.06.010-R8/star/bin/starccm+ -np 28 -machinefile ${HOSTS} -power -podkey ${PODKey} -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 14 -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java $SHARE_DATA/benchmark/*.sim' >> $SHARE_DATA/benchmark/runccm_example.sh
+chmod +x $SHARE_DATA/benchmark/runccm_example.sh
+chown $USER:$USER $SHARE_DATA/benchmark/runccm_example.sh
 
 cd $SHARE_DATA/INSTALLERS/starccm+_12.06.010/
 sh $SHARE_DATA/INSTALLERS/starccm+_12.06.010/STAR-CCM+12.06.010_01_linux-x86_64-2.5_gnu6.2-r8.sh -i silent -DINSTALLDIR=$SHARE_DATA/applications -DNODOC=true -DINSTALLFLEX=false
