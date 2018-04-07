707ef938345a95d71b74808c68718232c6f82d4f
hirtanak <hirtanak@gmail.com>
2018-04-07 11:00:29 +0900

    Update install-StarCCM-1206R8k.sh

--- install-StarCCM-1206R8k.sh
+++ install-StarCCM-1206R8k.sh
@@ -31,8 +31,12 @@ echo "export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.223/linux/mpi"
 echo "export PATH=$SHARE_DATA/applications/12.06.010-R8/STAR-CCM+12.06.010-R8/star/bin:/opt/intel/impi/5.1.3.223/bin64:$PATH" >> /home/$USER/.bashrc
 echo "export I_MPI_DYNAMIC_CONNECTION=0" >> /home/$USER/.bashrc
 echo "${SHARE_DATA}/applications/12.06.010-R8/STAR-CCM+12.06.010-R8/star/bin/starccm+ -np 28 -machinefile ${HOSTS} -power -podkey ${PODKey} -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 14 I_MPI_FABRICS=shm:dapl -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java ${SHARE_DATA}/benchmark/*.sim" >> $SHARE_DATA/benchmark/runccm_example.sh
+
+#setting privilege 
 chmod +x $SHARE_DATA/benchmark/runccm_example.sh
-chown $USER:$USER $SHARE_DATA/benchmark/runccm_example.shI_MPI_FABRICS=shm:dapl 
+chown $USER:$USER $SHARE_DATA/benchmark/runccm_example.sh
+chown $USER:$USER $SHARE_DATA/benchmark/*.sim
+chown $USER:$USER $SHARE_DATA/benchmark/*.tgz
 
 cd $SHARE_DATA/INSTALLERS/starccm+_12.06.010/
 sh $SHARE_DATA/INSTALLERS/starccm+_12.06.010/STAR-CCM+12.06.010_01_linux-x86_64-2.5_gnu6.2-r8.sh -i silent -DINSTALLDIR=$SHARE_DATA/applications -DNODOC=true -DINSTALLFLEX=false
