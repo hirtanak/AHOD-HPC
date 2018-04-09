8f787da1f108e04adef01b2dcd46f992544642a5
hirtanak <hirtanak@gmail.com>
2018-04-09 16:42:06 +0900

    Update install-StarCCM-1204.sh

--- install-StarCCM-1204.sh
+++ install-StarCCM-1204.sh
@@ -5,30 +5,39 @@ SHARE_HOME=$1
 LICIP=$2
 HOST=`hostname`
 DOWN=$3
+USER=$4
 SHARE_DATA=/mnt/resource/scratch
-echo $SHARE_HOME,$LICIP,$HOST,$DOWN,$SHARE_DATA
+echo $SHARE_HOME,$LICIP,$HOST,$DOWN,$SHARE_DATA,$USER
+
+# create directory
+mkdir -p $SHARE_DATA/INSTALLERS/starccm+_12.04.010/
 
 wget -q https://hirostpublicshare.blob.core.windows.net/solvers/STAR-CCM%2B12.04.010_01_linux-x86_64-r8.tar.gz -O $SHARE_DATA/INSTALLERS/STAR-CCM+12.04.010_01_linux-x86_64-r8.tar.gz
 #wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/runAndRecord.java -O $SHARE_DATA/benchmark/runAndRecord.java
-#wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/$DOWN -O $SHARE_DATA/benchmark/$DOWN
+wget -q https://storagekhi.blob.core.windows.net/khi2018storage/$DOWN -O $SHARE_DATA/benchmark/$DOWN
 
 #tar -xf $SHARE_DATA/benchmark/$DOWN -C $SHARE_DATA/benchmark
 tar -zxvf $SHARE_DATA/INSTALLERS/STAR-CCM+12.04.010_01_linux-x86_64-r8.tar.gz -C $SHARE_DATA/INSTALLERS/
 
-mkdir -p $SHARE_DATA/INSTALLERS/starccm+_12.04.010/
 cd $SHARE_DATA/INSTALLERS/starccm+_12.04.010/
 
-echo export PODKey=$LICIP >> $SHARE_HOME/.bashrc
-echo export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com >> $SHARE_HOME/.bashrc
-echo export HOSTS=$SHARE_HOME/bin/hosts
-#echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.223 >> $SHARE_HOME/.bashrc
-echo export I_MPI_FABRICS=shm:dapl >> $SHARE_HOME/.bashrc
-echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> $SHARE_HOME/.bashrc
-echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.223/linux/mpi >> $SHARE_HOME/.bashrc
-echo export PATH=$SHARE_DATA/applications/12.04.010/STAR-CCM+12.04.010/star/bin:/opt/intel/impi/5.1.3.223/bin64:$PATH >> $SHARE_HOME/.bashrc
-echo export I_MPI_DYNAMIC_CONNECTION=0 >> $SHARE_HOME/.bashrc
-echo '$SHARE_DATA/applications/12.04.010/STAR-CCM+12.04.010/star/bin/starccm+ -np 8 -machinefile '$HOSTS' -power -podkey '$PODKey' -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 8 -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java $SHARE_DATA/benchmark/*.sim' >> $SHARE_DATA/benchmark/runccm_example.sh
+echo "export PODKey=$LICIP" >> /home/$USER/.bashrc
+echo "export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com" >> /home/$USER/.bashrc
+#export HOSTS=$SHARE_HOME/bin/hosts
+echo "export I_MPI_FABRICS=shm:dapl" >> /home/$USER/.bashrc
+echo "export I_MPI_DAPL_PROVIDER=ofa-v2-ib0" >> /home/$USER/.bashrc
+echo "export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.223/linux/mpi" >> /home/$USER/.bashrc
+echo "export PATH=$SHARE_DATA/applications/12.04.010-R8/STAR-CCM+12.04.010-R8/star/bin:/opt/intel/impi/5.1.3.223/bin64:$PATH" >> /home/$USER/.bashrc
+echo "export I_MPI_DYNAMIC_CONNECTION=0" >> /home/$USER/.bashrc
+echo "${SHARE_DATA}/applications/12.04.010-R8/STAR-CCM+12.04.010-R8/star/bin/starccm+ -np 28 -machinefile ${SHARE_HOME}/hosts -licpath 1999@flex.cd-adapco.com -podkey $PODKey -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags '-ppn 14 -genv I_MPI_FABRICS=shm:dapl -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DYNAMIC_CONNECTION=0' -batch runAndRecord.java ${SHARE_DATA}/benchmark/*.sim" >> $SHARE_DATA/benchmark/runccm_example.sh
 
+#setting privilege 
+chmod +x $SHARE_DATA/benchmark/runccm_example.sh
+chown $USER:$USER $SHARE_DATA/benchmark/runccm_example.sh
+chown $USER:$USER $SHARE_DATA/benchmark/*.sim
+chown $USER:$USER $SHARE_DATA/benchmark/*.tgz
+
+cd $SHARE_DATA/INSTALLERS/starccm+_12.04.010/
 sh $SHARE_DATA/INSTALLERS/starccm+_12.04.010/STAR-CCM+12.04.010_01_linux-x86_64-2.5_gnu4.8.bin -i silent -DINSTALLDIR=$SHARE_DATA/applications -DNODOC=true -DINSTALLFLEX=false
 
 #rm -rf $SHARE_DATA/INSTALLERS/STAR-CCM+12.02.010_01_linux-x86_64.tar.gz
