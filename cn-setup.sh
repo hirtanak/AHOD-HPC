bea2e133ddb340905c36d787e17accba57ceb24f
hirtanak <hirtanak@gmail.com>
2018-06-20 08:44:25 +0900

    Update cn-setup.sh

--- cn-setup.sh
+++ cn-setup.sh
@@ -44,11 +44,11 @@ defaults 0 0" | tee -a /etc/fstab
     echo export PATH=/opt/intel/impi/${impi_version}/bin64:$PATH >> /home/$USER/.bashrc
     echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
     
-    chmod +x /etc/rc.d/rc.local
+    #chmod +x /etc/rc.d/rc.local
     #grep -v "touch" /etc/rc.d/rc.local | sed 's/touch/#touch/g' 
-    echo "sudo mkdir -p /mnt/resource/scratch" >> /etc/rc.d/rc.local
-    echo sleep 10 >> /etc/rc.d/rc.local
-    echo "sudo umount -a" >> /etc/rc.d/rc.local
+    #echo "sudo mkdir -p /mnt/resource/scratch" >> /etc/rc.d/rc.local
+    #echo sleep 10 >> /etc/rc.d/rc.local
+    #echo "sudo umount -a" >> /etc/rc.d/rc.local
     #echo "sudo mount -a" >> /etc/rc.d/rc.local
     #echo "exit 0" >> /etc/rc.d/rc.local
     
