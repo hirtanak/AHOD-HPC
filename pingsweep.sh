230eb6d2bca92bd15b7fd20c7d245b54ebe455e9
hirtanak <hirtanak@gmail.com>
2018-06-24 08:01:10 +0900

    Update pingsweep.sh

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Enter Your Sweep Subnet: "
 echo "Example: 10.0.0. for 10.0.0.0/24"
+echo "Enter Your Sweep Subnet: "
 read SUBNET
 
-for i in `seq 1 255`; do ping -c 1 ${SUBNET}.$i | tr \\n ' ' | awk '/1 received/ {print $2}'; done
+for i in `seq 1 255`; do ping -c 1 ${SUBNET}$i | tr \\n ' ' | awk '/1 received/ {print $2}'; done
