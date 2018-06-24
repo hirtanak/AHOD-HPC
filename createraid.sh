3a627f090114ecdfe612c3d6de6e3252699757cd
hirtanak <hirtanak@gmail.com>
2018-06-24 13:29:48 +0900

    Update createraid.sh

--- createraid.sh
+++ createraid.sh
@@ -5,13 +5,8 @@ sudo date; cat /etc/redhat-relase;
 echo "Enter number of disks. default is 8:"
 read DISKS
 
-##parted -s -a optimal /dev/sd command
-##DISKDRIVE=`echo $DISKS | tr "1-8" "c-z"`
-
 ENDDRIVE=$((66 + ${DISKS}))
-
 for i in `seq 67 ${ENDDRIVE}`; do
-echo $i
 DISKDRIVE=`printf "%b\n" $(printf "%s%x" "\\x" $i)`
 DISKDRIVE=`echo $DISKDRIVE | tr "C-Z" "c-z"`
 sudo parted -s -a optimal /dev/sd${DISKDRIVE} -- mklabel gpt   # GPT(GUID Partition Table)
